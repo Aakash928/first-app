6f7e9a5c579a73f523d96db2ea9052aaebc2c98c
Aakash928 <asassss337@gmail.com>
2022-05-16 19:57:56 +0530

    mobile os

---
+++ src/components/Answer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Mobile=()=>{
+
+    return <div>
+        <ul><h1>Mobile Operating System</h1>
+        <li>Android</li>
+        <li>Blackberry</li>
+        <li>iPhone</li>
+        <li>Windows</li>
+        </ul>
+        <ul><h1>Mobile Manufacturers</h1>
+        <li>Samsung</li>
+        <li>HTC</li>
+        <li>Micromax</li>
+        <li className="dot">Apple</li>
+
+
+        </ul>
+        </div>
+};
+export default Mobile;
