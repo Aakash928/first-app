aafe14742b42f4881fde85cefe48b18179d824bb
Aakash928 <asassss337@gmail.com>
2022-05-17 12:02:03 +0530

    a

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -3,20 +3,22 @@ import React from "react";
 const Mobile=()=>{
 
     return <div>
-        <ul><h1>Mobile Operating System</h1>
-        <li>Android</li>
-        <li>Blackberry</li>
-        <li>iPhone</li>
-        <li>Windows</li>
-        </ul>
-        <ul><h1>Mobile Manufacturers</h1>
-        <li>Samsung</li>
-        <li>HTC</li>
-        <li>Micromax</li>
-        <li className="dot">Apple</li>
+       <nav className="nav">
+           <div className="small a">
+               <h1>LOGOBAKERY</h1>
+           </div>
+           <div className="small b">
+               <h4>Services</h4>
+               <h4>Projects</h4>
+               <h4>About</h4>
+           </div>
+           <div className="small c">
+               <button className="button">Contact</button>
+           </div>
+       </nav>
+       </div>
 
-
-        </ul>
-        </div>
+       
+        
 };
 export default Mobile;
